afc089d33a1885714815f3feb6b8822e36e12d35
NguyenMinhQuang-260101 <quangnguyenminh2001@gmail.com>
2024-01-18 10:15:44 +0700

    Pagination - bind click event

--- js/home.js
+++ js/home.js
@@ -43,7 +43,7 @@ function createPostElement(post) {
     })
   }
 
-  // calculate timespan
+  // calculate timeSpan
   // console.log('timeSpan', dayjs(post.updatedAt).fromNow())
   setTextContent(liElement, '[data-id="timeSpan"]', ` - ${dayjs(post.updatedAt).fromNow()}`)
 
@@ -64,12 +64,61 @@ function renderPostList(postList) {
   })
 }
 
+function handleFilterChange(filterName, filterValue) {
+  // update query params
+  const url = new URL(window.location)
+  url.searchParams.set(filterName, filterValue)
+  history.pushState({}, '', url)
+
+  // fetch API
+  // re-render post list
+}
+
+function handlePrevClick(e) {
+  e.preventDefault()
+  console.log('prev click')
+}
+function handleNextClick(e) {
+  e.preventDefault()
+  console.log('next click')
+}
+
+function initPagination() {
+  // bind click event for prev/next
+  const ulPagination = document.getElementById('pagination')
+  if (!ulPagination) return
+
+  // add click event for prev link
+  const prevLink = ulPagination.firstElementChild?.firstElementChild
+  if (prevLink) {
+    prevLink.addEventListener('click', handlePrevClick)
+  }
+
+  const nextLink = ulPagination.lastElementChild?.lastElementChild
+  if (nextLink) {
+    nextLink.addEventListener('click', handleNextClick)
+  }
+}
+
+function initURL() {
+  const url = new URL(window.location)
+
+  // update search params if needed
+  if (!url.searchParams.get('_page')) url.searchParams.set('_page', 1)
+  if (!url.searchParams.get('_limit')) url.searchParams.set('_limit', 6)
+
+  history.pushState({}, '', url)
+}
+
 ;(async () => {
   try {
-    const queryParams = {
-      _page: 1,
-      _limit: 6,
-    }
+    initPagination()
+    initURL()
+
+    const queryParams = new URLSearchParams(window.location.search)
+    // set default query params if not existed
+    console.log(queryParams.toString())
+
     const { data, pagination } = await postApi.getAll(queryParams)
 
     renderPostList(data)
